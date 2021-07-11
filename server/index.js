e841855f1417b483814f5375cd48d8674dbbd6c5
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-12 00:23:55 +0530

    added passport

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-
+const passport = require("passport");
+const users = require("./routes/api/users");
 const { connectDB } = require("./config/db");
 
 // initialize server
@@ -8,6 +9,14 @@ const app = express();
 
 // connecting database
 connectDB();
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
+// routes
+app.use("/api/users", users);
 
 // initialize middleware
 app.use(express.json({ extended: false }));
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const mongoURI = process.env.MONGO_URI;
+const secretOrkey = process.env.secretOrKey;
 
 // Load input validation
 const { validateRegisterInput } = require("../../validation/register");
@@ -49,3 +49,58 @@ router.post("/register", (req, res) => {
         }
     });
 });
+
+/**
+ * @route POST api/users/login
+ * @description login user and return JWT token
+ * @access public
+ */
+
+router.post("/login", (req, res) => {
+    // form validation
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { email, password } = req.body;
+
+    // Find user by email
+    User.findOne({ email }).then((user) => {
+        // check if user exists
+        if (!user) {
+            return res.status(404).json({ message: "email not found" });
+        }
+        // check password
+        bcrypt.compare(password, user.password).then((isMatch) => {
+            if (isMatch) {
+                // user matched
+                // create JWT payload
+
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                };
+
+                // sign token
+                jwt.sign(
+                    payload,
+                    secretOrkey,
+                    {
+                        expiresIn: 31556926,
+                    },
+                    (err, token) => {
+                        res.json({ success: true, token: "Bearer " + token });
+                    }
+                );
+            } else {
+                return res.status(400).json({ message: "Incorrect password" });
+            }
+        });
+    });
+});
+
+module.exports = router;
