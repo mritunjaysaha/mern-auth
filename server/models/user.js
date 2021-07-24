d16fef80a08db96d6640bb6470e7f44821d438fd
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-24 17:58:41 +0530

    updated user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
     name: {
         type: String,
@@ -19,6 +20,6 @@ const UserSchema = new Schema({
     },
 });
 
-const User = mongoose.model("users", UserSchema);
+const User = mongoose.model("Users", UserSchema);
 
 module.exports = User;
